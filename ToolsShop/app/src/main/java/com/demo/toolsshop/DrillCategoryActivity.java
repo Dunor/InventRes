2a4e9d3402fa7c65e96af3b4464ca64aa556bab0
Dunor <dunor@mail.ru>
2020-08-03 20:29:04 +0700

    провера связи

---
+++ ToolsShop/app/src/main/java/com/demo/toolsshop/DrillCategoryActivity.java
@@ -0,0 +1,49 @@
+package com.demo.toolsshop;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class DrillCategoryActivity extends AppCompatActivity {
+
+    private ListView listViewDrills;
+
+    private ArrayList<Drill> drills;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drill_category);
+        listViewDrills = findViewById(R.id.listViewDrills);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        drills = new ArrayList<>();
+        drills.add(new Drill(getString(R.string.dill_interskol_title), getString(R.string.dill_interskol_info), R.drawable.interskol));
+        drills.add(new Drill(getString(R.string.dill_makita_title), getString(R.string.dill_makita_info), R.drawable.makita));
+        drills.add(new Drill(getString(R.string.dill_dewolt_title), getString(R.string.dill_dewolt_info), R.drawable.dewalt));
+        ArrayAdapter<Drill> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, drills);
+        listViewDrills.setAdapter(adapter);
+        listViewDrills.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Drill drill = drills.get(position);
+                Intent intent = new Intent(getApplicationContext(), DrillDetailActivity.class);
+                intent.putExtra("title", drill.getTitle());
+                intent.putExtra("info", drill.getInfo());
+                intent.putExtra("resId", drill.getImageResourceId());
+                startActivity(intent);
+
+            }
+        });
+    }
+}
